0a5b766b063ef5dbb3e0bc2a204b26c5590d9b73
sumudithavi <sumudithavi@gmail.com>
2011-06-28 10:35:46 +0000

    logical people group page's add button implemented

--- org.wso2.tools.humantask.editor/src/org/wso2/tools/humantask/editor/editors/pages/logicalpeoplegroups/AddParmWizard.java
+++ org.wso2.tools.humantask.editor/src/org/wso2/tools/humantask/editor/editors/pages/logicalpeoplegroups/AddParmWizard.java
@@ -1,12 +1,33 @@
 package org.wso2.tools.humantask.editor.editors.pages.logicalpeoplegroups;
 
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.edit.command.SetCommand;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.swt.widgets.Listener;
+import org.open.oasis.docs.ns.bpel4people.ws.humantask.ht.THumanInteractions;
+import org.open.oasis.docs.ns.bpel4people.ws.humantask.ht.TLogicalPeopleGroup;
+import org.open.oasis.docs.ns.bpel4people.ws.humantask.ht.TParameter;
+import org.open.oasis.docs.ns.bpel4people.ws.humantask.ht.htdFactory;
+import org.open.oasis.docs.ns.bpel4people.ws.humantask.ht.htdPackage;
 
 public class AddParmWizard extends Wizard{
 
 	private AddParmWizardPage page;
+	private THumanInteractions humanInteractions;
+	private EditingDomain domain;
+	private TableViewer tableviewer;
+	private LogicalPeopleGroupPage logicalPeopleGroupPage;
 	
-	
+	public AddParmWizard(THumanInteractions humanInteractions,EditingDomain domain,TableViewer tableviewer,LogicalPeopleGroupPage logicalPeopleGroupPage)
+	{
+		this.humanInteractions=humanInteractions;
+		this.domain=domain;
+		this.tableviewer=tableviewer;
+		this.logicalPeopleGroupPage=logicalPeopleGroupPage;
+	}
 	
 	public void addPages(){
 		page = new AddParmWizardPage();
@@ -16,8 +37,30 @@ public class AddParmWizard extends Wizard{
 	
 	@Override
 	public boolean performFinish() {
-		// TODO Auto-generated method stub
-		return false;
+		htdFactory factory= htdFactory.eINSTANCE;
+		TParameter tParameter= factory.createTParameter();
+	
+		logicalPeopleGroupPage.selectedLogicalPplGroup.getParameter().add(tParameter);
+		
+		
+		int size=logicalPeopleGroupPage.selectedLogicalPplGroup.getParameter().size();
+		setAttribute(htdPackage.eINSTANCE.getTParameter_Name(),page.getNameTextBoxContent(),size);
+		
+	
+		tableviewer.add(tParameter);
+		//tableviewer.setInput(hi_page.createModle());
+		return true;
+	}
+	
+	private void setAttribute(EAttribute tParameter_Attribute, String text,int size) {
+		Command setAttribCommand = SetCommand.create(domain, logicalPeopleGroupPage.selectedLogicalPplGroup.getParameter().get(size-1), tParameter_Attribute, text);
+
+		if (setAttribCommand.canExecute()) {
+			domain.getCommandStack().execute(setAttribCommand);
+			
+		} else {
+			System.out.println("can't modify Attribute: " + tParameter_Attribute.getName());
+		}
 	}
 	
 	public boolean performCancel()  {
