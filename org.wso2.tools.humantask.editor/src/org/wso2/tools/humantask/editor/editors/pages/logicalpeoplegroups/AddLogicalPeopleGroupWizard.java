dac79ea4e3bc76ba4549e874f83534de8c8ca23f
sumudithavi <sumudithavi@gmail.com>
2011-06-28 10:36:24 +0000

    logical people group page's add button implemented

--- org.wso2.tools.humantask.editor/src/org/wso2/tools/humantask/editor/editors/pages/logicalpeoplegroups/AddLogicalPeopleGroupWizard.java
+++ org.wso2.tools.humantask.editor/src/org/wso2/tools/humantask/editor/editors/pages/logicalpeoplegroups/AddLogicalPeopleGroupWizard.java
@@ -1,13 +1,29 @@
 package org.wso2.tools.humantask.editor.editors.pages.logicalpeoplegroups;
 
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.edit.command.SetCommand;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.wizard.Wizard;
+import org.open.oasis.docs.ns.bpel4people.ws.humantask.ht.TBoolean;
+import org.open.oasis.docs.ns.bpel4people.ws.humantask.ht.TExtension;
+import org.open.oasis.docs.ns.bpel4people.ws.humantask.ht.THumanInteractions;
+import org.open.oasis.docs.ns.bpel4people.ws.humantask.ht.TLogicalPeopleGroup;
+import org.open.oasis.docs.ns.bpel4people.ws.humantask.ht.htdFactory;
+import org.open.oasis.docs.ns.bpel4people.ws.humantask.ht.htdPackage;
 
 public class AddLogicalPeopleGroupWizard extends Wizard {
 
 	private AddLogicalPeopleGroupWizardPage page;
+	private THumanInteractions humanInteractions;
+	private EditingDomain domain;
+	private TableViewer tableviewer;
 	
-	public AddLogicalPeopleGroupWizard(){
-		
+	public AddLogicalPeopleGroupWizard(THumanInteractions humanInteractions,EditingDomain domain,TableViewer tableviewer){
+		this.humanInteractions=humanInteractions;
+		this.domain=domain;
+		this.tableviewer=tableviewer;
 	}
 	
 	public void addPages(){
@@ -18,8 +34,34 @@ public class AddLogicalPeopleGroupWizard extends Wizard {
 	
 	@Override
 	public boolean performFinish() {
-		// TODO Auto-generated method stub
-		return false;
+		htdFactory factory= htdFactory.eINSTANCE;
+		TLogicalPeopleGroup tLogicalPeopleGroup= factory.createTLogicalPeopleGroup();
+		
+		if(humanInteractions.getLogicalPeopleGroups()==null)
+		{
+			humanInteractions.setLogicalPeopleGroups(factory.createTLogicalPeopleGroups());
+			
+		}
+		humanInteractions.getLogicalPeopleGroups().getLogicalPeopleGroup().add(tLogicalPeopleGroup);
+		
+		int size=humanInteractions .getLogicalPeopleGroups().getLogicalPeopleGroup().size();
+		setAttribute(htdPackage.eINSTANCE.getTLogicalPeopleGroup_Name(),page.getNameTextBoxContent(),size);
+		
+	
+		tableviewer.add(tLogicalPeopleGroup);
+		//tableviewer.setInput(hi_page.createModle());
+		return true;
+	}
+	
+	private void setAttribute(EAttribute tLogicalPeopleGroup_Attribute, String text,int size) {
+		Command setAttribCommand = SetCommand.create(domain, humanInteractions.getLogicalPeopleGroups().getLogicalPeopleGroup().get(size-1), tLogicalPeopleGroup_Attribute, text);
+
+		if (setAttribCommand.canExecute()) {
+			domain.getCommandStack().execute(setAttribCommand);
+			
+		} else {
+			System.out.println("can't modify Attribute: " + tLogicalPeopleGroup_Attribute.getName());
+		}
 	}
 	
 	public boolean performCancel()  {
