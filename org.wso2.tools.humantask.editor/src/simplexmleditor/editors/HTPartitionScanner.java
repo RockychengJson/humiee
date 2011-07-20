122ead4b2b63260593f8d537aaab0b1b12a31df0
sumudithavi <sumudithavi@gmail.com>
2011-07-20 17:49:37 +0000

    for syntax coloring.package name must be changed to a valid name.

---
+++ org.wso2.tools.humantask.editor/src/simplexmleditor/editors/HTPartitionScanner.java
@@ -0,0 +1,21 @@
+package simplexmleditor.editors;
+
+import org.eclipse.jface.text.rules.*;
+
+public class HTPartitionScanner extends RuleBasedPartitionScanner {
+	public final static String XML_COMMENT = "__xml_comment";
+	public final static String XML_TAG = "__xml_tag";
+
+	public HTPartitionScanner() {
+
+		IToken xmlComment = new Token(XML_COMMENT);
+		IToken tag = new Token(XML_TAG);
+
+		IPredicateRule[] rules = new IPredicateRule[2];
+
+		rules[0] = new MultiLineRule("<!--", "-->", xmlComment);
+		rules[1] = new TagRule(tag);
+
+		setPredicateRules(rules);
+	}
+}
