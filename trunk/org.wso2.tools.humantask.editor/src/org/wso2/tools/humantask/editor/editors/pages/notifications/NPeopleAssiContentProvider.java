ddd3dad7e97864fcf1b04bd484bbe67a9925dda5
sumudithavi@gmail.com <sumudithavi@gmail.com@2a681c7c-26ca-7503-394d-a2ed09ff3ac9>
2011-08-10 09:17:38 +0000

    people assignment table completed

--- trunk/org.wso2.tools.humantask.editor/src/org/wso2/tools/humantask/editor/editors/pages/notifications/NPeopleAssiContentProvider.java
+++ trunk/org.wso2.tools.humantask.editor/src/org/wso2/tools/humantask/editor/editors/pages/notifications/NPeopleAssiContentProvider.java
@@ -1,8 +1,13 @@
 package org.wso2.tools.humantask.editor.editors.pages.notifications;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.Viewer;
 import org.open.oasis.docs.ns.bpel4people.ws.humantask.ht.TNotification;
+import org.open.oasis.docs.ns.bpel4people.ws.humantask.ht.TTask;
+import org.wso2.tools.humantask.editor.editors.pages.task.HumanRole;
 
 public class NPeopleAssiContentProvider implements IStructuredContentProvider {
 
@@ -21,9 +26,40 @@ public class NPeopleAssiContentProvider implements IStructuredContentProvider {
 	@Override
 	public Object[] getElements(Object inputElement) {
 		
-		TNotification notification = (TNotification)inputElement;
+		TNotification t=(TNotification)inputElement;
+		List<HumanRole> list=new ArrayList<HumanRole>();
+		//list.add(t);*/
+		
+		if(t.getPeopleAssignments()!=null)
+		{
+		if(t.getPeopleAssignments().getBusinessAdministrators()!=null)
+		{
+			for(int i=0;i<t.getPeopleAssignments().getBusinessAdministrators().size();++i)
+			{
+				list.add(new HumanRole(t.getPeopleAssignments().getBusinessAdministrators().get(i),"BusinessAdministrators",i));
+			}
+		}
+		
+		
+		if(t.getPeopleAssignments().getRecipients()!=null)
+		{
+			for(int i=0;i<t.getPeopleAssignments().getRecipients().size();++i)
+			{
+				list.add(new HumanRole(t.getPeopleAssignments().getRecipients().get(i), "Recipients",i));
+			}
+		}
+		
+		}
+		
+		return list.toArray();
+		
+		
+		
+	/*	TNotification notification = (TNotification)inputElement;
 		//TODO 
-		return notification.getPeopleAssignments().getBusinessAdministrators().toArray();
-	}
+		return notification.getPeopleAssignments().getBusinessAdministrators().toArray();*/
+	
 
+
+}
 }
